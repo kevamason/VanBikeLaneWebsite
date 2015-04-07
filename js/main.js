2f74f804ef9563fd0480be51b92d6cdaa39d0009
Kevin Mason <kev.a.mason@gmail.com>
2015-04-06 23:01:37 -0700

    Deployed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
 
-var $window = $(window); //You forgot this line in the above example
+	var $window = $(window); //You forgot this line in the above example
 
-$('section[data-type="background"]').each(function(){
-var $bgobj = $(this); // assigning the object
-$(window).scroll(function() {
-	var yPos = -( ($window.scrollTop())/ $bgobj.data('speed'));
-// Put together our final background position
-var coords = '50% '+ yPos + 'px';
-// Move the background
-$bgobj.css({ backgroundPosition: coords });
-});
-});
+	$('section[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		$(window).scroll(function() {
+			var yPos = -( ($window.scrollTop())/ $bgobj.data('speed'));
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		var target = this.hash;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top-30
+		}, 500, 'swing', function () {
+			window.location.hash = target;
+			});
+	});
 });
